5b7e21fa8da669537e344e499e1d7c8ae8a2e5a4
dj 2828 <dj28partystudio@gmail.com>
2025-12-27 13:54:26 +0100

    Update main.py

--- main.py
+++ main.py
@@ -44,7 +44,8 @@ def load_music_folders(path_txt):
                 full_path = os.path.join(root, d)
                 music_folders[d] = full_path
         music_folders["YouTube"] = YT_FOLDER
-        ALBUM_ORDER = [a.strip() for a in os.getenv("ALBUM_ORDER", "").split("=")[1].split(',')]
+        valueA = os.getenv("ALBUM_ORDER", "").split('=')[1]
+        ALBUM_ORDER = [a.strip() for a in valueA.split(',')]
     return music_folders
 def get_music():
     songs_by_folder = {}
